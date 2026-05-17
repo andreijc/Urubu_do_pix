efc92cdbe27fb74c746ee5bad013a447c354fad1
andreijc <andreicruz1203@gmail.com>
2026-05-16 22:03:42 -0300

    Reorganizei tudo

--- UrubudoPIX/wsgi.py
+++ UrubudoPIX/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for urubuDoPix project.
+WSGI config for UrubudoPIX project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'urubuDoPix.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UrubudoPIX.settings')
 
 application = get_wsgi_application()
